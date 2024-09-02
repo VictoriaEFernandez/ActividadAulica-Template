543e5fe27bd085ce60ebd4984549317e264cd71a
VictoriaEFernandez <vicki311fdz@gmail.com>
2024-09-02 19:16:05 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog De Adopcion</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nerko+One&family=Shadows+Into+Light&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!--Encabezado-->
+    <header>
+        <h1>Blog de Adopcion</h1>
+        <nav> <!--Menu de navegacion-->
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Entradas</a></li>
+                <li><a href="#">Galeria</a></li>
+                <li><a href="#">Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <!--Seccion Principal y Noticias-->
+    <main>
+        <div class="contenedor-central">
+        <section id="principal">
+            <img src="img/principal.jpeg" alt="Imagen Principal">
+            <div class="texto-principal">
+                <h2>Tejiendo Lazos</h2>
+                <p>Aquí encontrarás información detallada sobre adopciones y cómo puedes contribuir a hacer la diferencia. Desde los pasos iniciales para adoptar hasta cómo apoyar a organizaciones que se dedican a encontrar hogares para aquellos que más lo necesitan. Te guiaremos a través del proceso, responderemos a tus preguntas, y te brindaremos recursos para que puedas participar activamente en este noble acto de amor. Juntos, podemos crear un futuro mejor para todos los que están buscando un hogar lleno de cariño. ¡Descubre cómo puedes ayudar hoy mismo!</p>
+                <a href="#" class="boton">Leer mas</a>
+            </div>
+        </div>
+        </section>
+        <div id="contenedor-principal">
+            <section id="contenido">
+                <h2>Bienvenidos a nuestro sitio</h2>
+                <p>Este es un blog dedicado a la adopción de mascotas. Aquí encontrarás historias inspiradoras y consejos útiles.</p>
+                <ul>
+                    <p>Numeros para mas informacion</p>
+                    <li>159753624</li>
+                    <li>12345-9867</li>
+                    <li>75335-9512</li>
+                    <li>89765-45769</li>
+                </ul>
+                <p>Adoptar una mascota no solo brinda un hogar a un animal que lo necesita, sino que también enriquece nuestras vidas con amor y compañía incondicional. A través de este sitio, queremos inspirarte a dar ese gran paso, ofreciéndote recursos, consejos, y las historias de éxito de aquellos que ya han encontrado a su amigo peludo ideal. ¡Únete a nuestra comunidad y descubre el impacto positivo que una mascota puede tener en tu vida!</p>
+                <div class="texto-linea-vertical">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, architecto ducimus? Velit suscipit vel repellendus quod obcaecati, aliquid ea, magnam sunt accusantium dolore reprehenderit exercitationem qui rem perspiciatis tempora temporibus!</p>
+                </div>
+            </section>
+                <!--Noticias Recientes-->
+                <section id="noticias">
+                    <h2>Adopciones Recientes</h2>
+                    <article>
+                        <img src="img/noticia1.jpeg" alt="Adopcion Reciente 1">
+                        <h3>Titulo Noticia</h3>
+                        <p>Descripcion Noticia</p>
+                    </article>
+                    <article>
+                        <img src="img/noticia2.jpeg" alt="Adopcion Reciente 2">
+                        <h3>Titulo Noticia</h3>
+                        <p>Descripcion Noticia</p>
+                    </article>
+                    <article>
+                        <img src="img/noticia3.jpeg" alt="Adopcion Reciente 3">
+                        <h3>Titulo Noticia</h3>
+                        <p>Descripcion Noticia</p>
+                    </article>
+                </section>
+        </div>
+    </main>
+    <!--Pie de Pagina-->
+    <footer>
+    <div>
+        <h3>Servicios</h3>
+        <ul>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Cum sociis</a></li>
+            <li><a href="#">Donec quam</a></li>
+            <li><a href="#">Nulla consequat</a></li>
+            <li><a href="#">In enim justo</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Acerca de</h3>
+        <ul>
+            <li><a href="#">Nullam quis</a></li>
+            <li><a href="#">Sed consequat</a></li>
+            <li><a href="#">Cras dapibus</a></li>
+            <li><a href="#">Lorem ipsum dolor</a></li>
+            <li><a href="#">Cum sociis</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Enlaces útiles</h3>
+        <ul>
+            <li><a href="#">Aenean vulputate</a></li>
+            <li><a href="#">Etiam ultricies</a></li>
+            <li><a href="#">Nullam quis</a></li>
+            <li><a href="#">Sed consequat</a></li>
+            <li><a href="#">Cras dapibus</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Contacto</h3>
+        <ul>
+            <li><a href="#">Donec quam</a></li>
+            <li><a href="#">Nulla consequat</a></li>
+            <li><a href="#">In enim justo</a></li>
+            <li><a href="#">Aenean vulputate</a></li>
+            <li><a href="#">Etiam ultricies</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Socios</h3>
+        <ul>
+            <li><a href="#">Plantillas CSS</a></li>
+            <li><a href="#">Sitios Flash</a></li>
+            <li><a href="#">Temas de WordPress</a></li>
+            <li><a href="#">Consejos de diseño web</a></li>
+            <li><a href="#">Fotos de stock gratis</a></li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+</html>
